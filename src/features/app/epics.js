3996f2c2cc04c501dfe78c18e87a5e9f78d4765e
Jakub Małojło <j.malojlo@gmail.com>
2019-04-22 16:03:26 +0200

    filter out undefined callbacks

--- src/features/app/epics.js
+++ src/features/app/epics.js
@@ -5,18 +5,21 @@ import { switchMap, map } from "rxjs/operators";
 
 import * as appActions from "./actions";
 
-function spreadFuncIf(func, ...args) {
-  return func ? [func(...args)] : [];
-}
-
 const getUsers = action$ =>
   action$
     .ofType(appActions.GET_USERS_REQUEST)
     .pipe(
       switchMap(action =>
-        from(axios.get("https://jsonplaceholder.typicode.com/users").then(res => res.data)).pipe(
+        from(
+          axios
+            .get("https://jsonplaceholder.typicode.com/users")
+            .then(res => res.data)
+        ).pipe(
           switchMap(users =>
-            from([appActions.getUsersSuccess(users), action.meta.onComplete(users)])
+            from([
+              appActions.getUsersSuccess(users),
+              ...(action.meta.onComplete ? [action.meta.onComplete(users)] : [])
+            ])
           )
         )
       )
@@ -29,7 +32,11 @@ const getPostsForUser = action$ =>
       switchMap(action =>
         from(
           axios
-            .get(`https://jsonplaceholder.typicode.com/posts?userId=${action.payload.userId}`)
+            .get(
+              `https://jsonplaceholder.typicode.com/posts?userId=${
+                action.payload.userId
+              }`
+            )
             .then(res => res.data)
         ).pipe(map(users => appActions.getPostsForUserSuccess(users)))
       )
